fd8c411db4b2218158214e4671e6219c40c457d3
rndpatel@gmail.com <rndpatel@gmail.com>
2019-11-15 10:05:18 -0800

    save/edit added // PUT request added

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -22,17 +22,32 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+      updateColors(
+          colors.map(color => {
+              if (color.id === colorToEdit.id) {
+                  return colorToEdit;
+              } else {
+                  return color;
+              }
+          })
+      );
+      axiosWithAuth()
+          .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+          .then(res => {
+              console.log("Edited color on Server", res);
+          })
+          .catch(err => console.log(err.response));
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
     //Update Local state
-    updateColors(colors.filter(item => item.id !== color.id));
+      updateColors(colors.filter(item => item.id !== color.id));
     //Perform Delete Reqest
     axiosWithAuth()
         .delete(`http://localhost:5000/api/colors/${color.id}`)
         .then(res => {
-          console.log(res);
+            console.log("Deleted a color on server", res);
         })
         .catch(err => console.log("error in deleting: ", err.response));
   };
