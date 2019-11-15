b911711415b9f392b01f19649d9a615ec4747820
rndpatel@gmail.com <rndpatel@gmail.com>
2019-11-15 09:54:38 -0800

    PrivateRoute added to BubblePage / axiosWithAuth working

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,20 +1,30 @@
+
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+    const [colorList, setColorList] = useState([]);
+    // fetch your colors data from the server when the component mounts
+    // set that data to the colorList state property
 
-  return (
-    <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
-    </>
-  );
+    useEffect(() => {
+        axiosWithAuth()
+            .get(`http://localhost:5000/api/colors`)
+            .then(res => {
+                setColorList(res.data);
+            })
+            .catch(err => console.log(err.response));
+    }, []);
+
+    return (
+        <>
+            <ColorList colors={colorList} updateColors={setColorList} />
+            <Bubbles colors={colorList} />
+        </>
+    );
 };
 
 export default BubblePage;
